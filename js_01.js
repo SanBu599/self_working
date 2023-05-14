2eddb89cd7211ce69b7c98ec97bee35c1abf4388
SanBu599 <112762794+SanBu599@users.noreply.github.com>
2023-05-14 20:32:58 +0900

    0514_01

--- js_01.js
+++ js_01.js
@@ -1,4 +1,23 @@
+// var name = 10;
+// var mane = 20;
+// console.log(name+mane)
 
-var num = 10 + 20;
 
-console.log(num);
+// let num = 10;
+// console.log(num);
+
+// var score = 10;
+// console.log(score)
+
+// let studentScore = {
+// koreanScore: 80,
+// englishScore: 70,
+// mathScore: 90,
+// scienceScore: 60,
+// };
+
+// console.log(studentScore.koreanScore);
+
+let score = 90;
+let grade = score >= 90 ? "A+" : "B-"
+console.log(grade);
